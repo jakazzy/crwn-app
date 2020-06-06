60c5e962413548c9fedf4a37ffae78560216f7c5
jakazzy <jakazzy@gmail.com>
2020-06-06 01:45:41 +0000

    add dynamic routing to menu items

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,22 @@
 import React from 'react'
+import { withRouter } from 'react-router-dom'
 import './menu-item.styles.scss'
 
-const menuItem =({ title, imageUrl, size }) =>{
+const MenuItem =({ title, imageUrl, size, history, linkUrl, match }) =>{
     return (
-        <div 
-        style={{ 
-            backgroundImage: `url(${imageUrl})`
-        }}
-        className={`${size} menu-item`}>
-        <div className='content'>
-          <div className='title'>{ title }</div>
+        <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+          <div
+            className='background-image'
+            style={{ 
+              backgroundImage: `url(${imageUrl})`
+            }}
+          />
+          <div className='content'>
+          <div className='title'>{ title.toUpperCase() }</div>
           <span className='subtitle'>SHOP NOW</span>
         </div>
       </div>
     )
 }
 
-export default menuItem
+export default withRouter(MenuItem)
