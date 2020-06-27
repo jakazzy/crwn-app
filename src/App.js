4ea3592e6915e2fc75676be778a71a278c87b473
jakazzy <jakazzy@gmail.com>
2020-06-27 17:45:41 +0000

    set user to null

--- src/App.js
+++ src/App.js
@@ -22,9 +22,21 @@ export class App extends Component {
   unsubscribeFromAuth = null
 
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      createUserProfileDocument(user)
-      
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth)
+        userRef.onSnapshot(snapshot =>{
+          this.setState({
+            currentUser: {
+              id: snapshot.id,
+              ...snapshot.data()
+            }
+          })
+        })
+      }
+      this.setState({
+        currentUser: userAuth
+      })
     })
   }
 
