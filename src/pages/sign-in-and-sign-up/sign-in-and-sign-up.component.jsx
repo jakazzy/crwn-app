01908fe2ef6b7295abd79044f3f4eff47e99fdcf
jakazzy <jakazzy@gmail.com>
2020-06-27 22:05:15 +0000

    add signup with email and password

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import SignIn from '../../components/sign-in/sign-in.component'
+import React from "react";
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
-import './sign-in-and-sign-up.styles.scss'
-const signInAndSignUpPage=()=> {
-    return (
-        <div className='sign-in-and-sign-up'>
-            <SignIn/>
-        </div>
-    )
-}
+import "./sign-in-and-sign-up.styles.scss";
+const signInAndSignUpPage = () => {
+  return (
+    <div className="sign-in-and-sign-up">
+      <SignIn />
+      <SignUp />
+    </div>
+  );
+};
 
-export default signInAndSignUpPage
+export default signInAndSignUpPage;
