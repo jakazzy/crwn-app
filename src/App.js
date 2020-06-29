a93a043bf89b0e8149bc6bd8997683502c8dcb86
jakazzy <jakazzy@gmail.com>
2020-06-29 10:57:41 +0000

    fix error with reducx

--- src/App.js
+++ src/App.js
@@ -14,10 +14,11 @@ import { setCurrentUser }from './redux/user/user.actions'
 
 
 export class App extends Component {
-
+  
   unsubscribeFromAuth = null
 
   componentDidMount(){
+    const {setCurrentUser} = this.props
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth)
@@ -30,9 +31,7 @@ export class App extends Component {
           
         })
       }
-      this.setState({
-        currentUser: userAuth
-      })
+      setCurrentUser({ userAuth})
     })
   }
 
