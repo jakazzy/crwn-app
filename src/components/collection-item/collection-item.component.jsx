c5f7e414ea37498e7e963b8a1aaf345022089c16
jakazzy <jakazzy@gmail.com>
2020-06-29 16:11:55 +0000

    create the add to cart button

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,20 +1,19 @@
-import React from 'react'
+import React from "react";
 
-import './collection-item.styles.scss'
+import CustomButton from "../custom-button/custom-button.component";
+import "./collection-item.styles.scss";
 
-const CollectionItem = ({id, name, price, imageUrl}) =>{
-    return (
-        <div className='collection-item'>
-            <div
-            className='image'
-            style={{ backgroundImage: `url(${imageUrl})`}}
-            />
-            <div className='collection-footer'>
-              <span className='name'>{name}</span>
-              <span className='price'>{price}</span>
-            </div>      
-        </div>
-    )
-}
+const CollectionItem = ({ id, name, price, imageUrl }) => {
+  return (
+    <div className="collection-item">
+      <div className="image" style={{ backgroundImage: `url(${imageUrl})` }} />
+      <div className="collection-footer">
+        <span className="name">{name}</span>
+        <span className="price">{price}</span>
+      </div>
+      <CustomButton inverted> Add to cart</CustomButton>
+    </div>
+  );
+};
 
-export default CollectionItem
+export default CollectionItem;
