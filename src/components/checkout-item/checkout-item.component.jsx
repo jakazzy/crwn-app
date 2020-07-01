833b8dde54e1d72f02198cce2496fafcc2255226
jakazzy <jakazzy@gmail.com>
2020-07-01 14:47:51 +0000

    add arrow to the checkitem quantity on the checkout page

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -1,19 +1,32 @@
 import React from "react";
+import { connect } from "react-redux";
 
+import { clearItemFromCart } from "../../redux/cart/cart.actions";
 import "./checkout-item.styles.scss";
 
-const checkoutItem = ({ cartItem: { name, imageUrl, price, quantity } }) => {
+const checkoutItem = ({ cartItem, clearItem }) => {
+  const { name, imageUrl, price, quantity } = cartItem;
   return (
     <div className="checkout-item">
       <div className="image-container">
         <img src={imageUrl} alt="item" />
       </div>
       <span className="name"> {name} </span>
-      <span className="quantity"> {quantity} </span>
+      <span className="quantity">
+        <div className="arrow">&#10094;</div>
+        <span className="value">{quantity}</span>
+        <div className="arrow"> &#10095;</div>
+      </span>
       <span className="price"> {price} </span>
-      <div className="remove-button"> &#10005;</div>
+      <div className="remove-button" onClick={() => clearItem(cartItem)}>
+        {" "}
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default checkoutItem;
+const mapDispatchToProps = (dispatch) => ({
+  clearItem: (item) => dispatch(clearItemFromCart(item)),
+});
+export default connect(null, mapDispatchToProps)(checkoutItem);
