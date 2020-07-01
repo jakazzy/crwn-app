8ad9d78b9915f34793b3793b0297cf0c5f96bc4a
jakazzy <jakazzy@gmail.com>
2020-07-01 00:34:02 +0000

    add cart item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,19 +1,20 @@
-import React from 'react'
-import CollectionItem from '../collection-item/collection-item.component'
-import './collection-preview.styles.scss'
+import React from "react";
+import CollectionItem from "../collection-item/collection-item.component";
+import "./collection-preview.styles.scss";
 
-const CollectionPreview =({ title, items}) =>{
-    return (
-        <div className ='collection-preview'>
-            <h1 className='title'>{ title.toUpperCase() }</h1>
-            <div className='preview'>
-              { items.filter((item, idx)=>idx < 4).map(({id, ...otherItemProps}) => (
-                  <CollectionItem key={id} { ...otherItemProps}/>
-              ))}
-            </div>
-        </div>
-    )
-}
+const CollectionPreview = ({ title, items }) => {
+  return (
+    <div className="collection-preview">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <div className="preview">
+        {items
+          .filter((item, idx) => idx < 4)
+          .map((item) => (
+            <CollectionItem key={item.id} item={item} />
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default CollectionPreview
-
+export default CollectionPreview;
