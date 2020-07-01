1fe3da9bc392acaaa0c1ef8e55ef6062d884061e
jakazzy <jakazzy@gmail.com>
2020-07-01 00:44:29 +0000

    fix error with the cartReducer

--- src/redux/cart/cart.reducer.jsx
+++ src/redux/cart/cart.reducer.jsx
@@ -16,7 +16,7 @@ const cartReducer = (state = INITIAL_STATE, action) => {
     case CartActionTypes.ADD_ITEM:
       return {
         ...state,
-        cartItem: [...state.cartItems, action.payload],
+        cartItems: [...state.cartItems, action.payload],
       };
     default:
       return state;
